498007d0880ee5915e0dc19c8cc43907a13ad2fe
Asal himsanda <150653318+asalhanadapangoda@users.noreply.github.com>
2025-07-01 15:52:44 +0530

    update

--- header.php
+++ header.php
@@ -7,13 +7,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Uniconnect</title>
-<link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <ul>
   <li><a href="index.php">Home</a></li>
   <li><a href="news.php">News</a></li>
-  <li><a href="contact.asp">About</a></li>
   <li style="float:right"><a class="active" href="login.php">Logout</a></li>
 </ul>
 <div class="container" style="margin:20px;">
